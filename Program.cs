ca2da8d3cfd4b0edb28845137fc7c4d7be87ce5a
ilja.tihhonov <ilja.tihhonov@outlook.com>
2022-06-05 22:47:01 +0300

    Celsius-Fahrenheit converter

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CelsiusFahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ToFahrenheit();
+            ToCelsius();
+        }
+       
+        public static void ToFahrenheit()
+        {
+            Console.WriteLine("Enter temerature in Celsius:");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+            int fahrenheit = (celsius * 9) / 5 + 32;
+            Console.WriteLine(fahrenheit);
+        }
+        public static void ToCelsius()
+        {
+            Console.WriteLine("Enter temerature in Fahrenheit:");
+            int fahrenheit = Convert.ToInt32(Console.ReadLine());
+            int celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine(celsius);
+        }
+    }
+}
